7da279a1384a42c4b7f1758bc8e0351a2ce1c8b2
David <davidmillett@protonmail.com>
2025-09-19 14:42:43 +0100

    populated landing page and created menufunctions

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react"
+import Controls from "../../components/Controls/Controls"
+
 const LandingPage = () => {
+
+    // Variables
+    const menu = ['pokedex', 'party']
+    const [itemCurrent, setItemCurrent] = useState(0)
+
+    // Functions
+    const menuScrollDown = () => {
+        setItemCurrent(itemCurrent + 1)
+    }
+
+    const menuScrollUp = () => {
+        setItemCurrent(itemCurrent - 1)
+    }
+
+    // Params
+    const buttonFunctions = {
+        up: {function: menuScrollUp, disabled: itemCurrent === 0},
+        right: {function: null, disabled: true},
+        down: {function: menuScrollDown, disabled: itemCurrent === menu.length - 1},
+        left: {function: null, disabled: true},
+        a: null,
+        b: null
+    }
+
     return (
         <main>
             <div className="screen">
                 <h1>Welcome</h1>
                 <ul>
-                    <li>Pokedex</li>
-                    <li>Party</li>
+                    {menu.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })}
                 </ul>
             </div>
+            <Controls buttonFunctions={buttonFunctions} />
         </main>
     )
 }
